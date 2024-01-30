f64d3c70b94369ee480973dd951538a262ffd20d
julicastro <castroejulian@gmail.com>
2024-01-30 06:02:15 -0300

    commit

--- Lab4_CrearDBTablas.sql
+++ Lab4_CrearDBTablas.sql
@@ -67,20 +67,73 @@ CREATE TABLE Discos.Stock (
     precio decimal(5, 2) null,
     cantidad int null,
     CONSTRAINT PK_Stock PRIMARY KEY CLUSTERED (itemno), 
-    CONSTRAINT FK_Stock_Album FOREIGN KEY (itemno) REFERENCES Discos.Artista(artno)
+    CONSTRAINT FK_Stock_Album FOREIGN KEY (itemno) REFERENCES Discos.Album(itemno)
 );
 
 CREATE TABLE Discos.Orden(
 	itemno smallint not null,
 	timestamp timestamp null,
 	CONSTRAINT PK_Orden PRIMARY KEY CLUSTERED (itemno), 
-    CONSTRAINT FK_Orden_Stock FOREIGN KEY (itemno) REFERENCES Discos.Artista(artno)
+    CONSTRAINT FK_Orden_Stock FOREIGN KEY (itemno) REFERENCES Discos.Stock(itemno)
 )
 
-/* 6. Realizar los siguientes cambios en el modelo: 
-6.1. Cambiar el tamaño de campo ciudad en la tabla ciudad para que sea de 
-30 en lugar de 25. 
-6.2. En la tabla de Stock, colocar el precio con un valor por defecto en 0 
-(cero). 
-6.3. En la tabla de álbumes el nombre del título no puede ser nulo. */
 
+/* 6. Realizar los siguientes cambios en el modelo: */
+/* 6.1. Cambiar el tamaño de campo ciudad en la tabla ciudad para que sea de 
+30 en lugar de 25. */
+
+ALTER TABLE Discos.Concierto
+	ALTER COLUMN ciudad varchar(30);
+
+/* 6.2. En la tabla de Stock, colocar el precio con un valor por defecto en 0 
+(cero). */
+
+ALTER TABLE Discos.Stock
+	ADD CONSTRAINT df_precio
+	DEFAULT 0 FOR precio;
+
+/* obtener todos los datos de la columna de una tabla */ 
+SELECT *
+	FROM sys.columns
+	WHERE object_id = OBJECT_ID('Discos.Stock');
+
+/* 6.3. En la tabla de álbumes el nombre del título no puede ser nulo. */
+ALTER TABLE Discos.Album
+	ALTER COLUMN titulo varchar(50) not null;
+
+/* 7. Agregar los siguientes registros dentro de la base de datos creada: 
+- 3 artistas 
+- 2 conciertos por cada uno de los artistas en diferentes fechas y ciudades 
+- 2 álbumes por cada uno de los artistas 
+- Stock sólo de 2 álbumes de diferentes artistas */
+
+INSERT INTO Discos.Artista (artno, nombre, clasificacion, bio, foto) VALUES (1, 'Juan Carlos', 'R', 'Es un fenomeno', null);
+INSERT INTO Discos.Artista (artno, nombre, clasificacion, bio, foto) VALUES (2, 'Lionel Messi', 'H', 'Un maestro total', null);
+INSERT INTO Discos.Artista (artno, nombre, clasificacion, bio, foto) VALUES (3, 'Carol Sanchez', 'P', 'Na es tremendo', null);
+
+INSERT INTO Discos.Concierto (artno, fecha, ciudad)
+VALUES 
+    (1, '2024-07-15', 'Haedo'),
+    (1, '2024-08-20', 'Morón'),
+    (2, '2024-09-10', 'Castelar'),
+    (2, '2024-10-05', 'Ituzaingó'),
+    (3, '2024-11-15', 'Ramos Mejía'),
+    (3, '2024-12-02', 'San Justo');
+
+INSERT INTO Discos.Album (titulo, artno, itemno)
+VALUES 
+    ('Álbum1_Artista1', 1, 1),
+    ('Álbum2_Artista1', 1, 2),
+    ('Álbum1_Artista2', 2, 3),
+    ('Álbum2_Artista2', 2, 4),
+    ('Álbum1_Artista3', 3, 5),
+    ('Álbum2_Artista3', 3, 6);
+
+INSERT INTO Discos.Stock (itemno, tipo, precio, cantidad)
+VALUES 
+    (1, 'A', 10.99, 50),
+    (2, 'B', 15.99, 30),
+    (3, 'C', 12.99, 40),
+    (4, 'D', 18.99, 20),
+    (5, 'E', 14.99, 45),
+    (6, 'F', 20.99, 25);
