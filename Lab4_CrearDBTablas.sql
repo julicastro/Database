7c8f4562a6711e2da22478b977cf93e8bc42b26d
julicastro <castroejulian@gmail.com>
2024-01-30 04:54:32 -0300

    commit

--- Lab4_CrearDBTablas.sql
+++ Lab4_CrearDBTablas.sql
@@ -31,8 +31,56 @@ datos creada:
 -- RESPONDIDO EN EL WORD
 
 /* 3. Crear el esquema discos. */
-CREATE SCHEMA Discos; 
+-- CREATE SCHEMA Discos; 
 
 /* 4. Se desea crear el siguiente modelo relacional. Recordar que se deben crear 
 cada una de las tablas involucradas y de sus relaciones.*/
+
+CREATE TABLE Discos.Artista( 
+	artno smallint NOT NULL, 
+	nombre varchar(50) NULL, 
+	clasificacion char(1) NULL, 
+	bio text NULL, 
+	foto image null, 
+	CONSTRAINT PK_Artista PRIMARY KEY CLUSTERED (artno) 
+);
+
+CREATE TABLE Discos.Concierto(
+	artno smallint not null,
+	fecha datetime not null,
+	ciudad varchar(25) not null,
+	CONSTRAINT PK_Concierto PRIMARY KEY CLUSTERED (fecha), 
+	CONSTRAINT FK_Concierto_Artista FOREIGN KEY (artno) REFERENCES Discos.Artista(artno)
+);
+
+CREATE TABLE Discos.Album (
+	titulo VARCHAR(50) not null,
+	artno smallint not null,
+	itemno smallint not null,
+	CONSTRAINT PK_Album PRIMARY KEY CLUSTERED (itemno), 
+	CONSTRAINT FK_Album_Artista FOREIGN KEY (artno) REFERENCES Discos.Artista(artno)
+);
+
+CREATE TABLE Discos.Stock (
+    itemno smallint not null,
+    tipo char(1) null,
+    precio decimal(5, 2) null,
+    cantidad int null,
+    CONSTRAINT PK_Stock PRIMARY KEY CLUSTERED (itemno), 
+    CONSTRAINT FK_Stock_Album FOREIGN KEY (itemno) REFERENCES Discos.Artista(artno)
+);
+
+CREATE TABLE Discos.Orden(
+	itemno smallint not null,
+	timestamp timestamp null,
+	CONSTRAINT PK_Orden PRIMARY KEY CLUSTERED (itemno), 
+    CONSTRAINT FK_Orden_Stock FOREIGN KEY (itemno) REFERENCES Discos.Artista(artno)
+)
+
+/* 6. Realizar los siguientes cambios en el modelo: 
+6.1. Cambiar el tamaño de campo ciudad en la tabla ciudad para que sea de 
+30 en lugar de 25. 
+6.2. En la tabla de Stock, colocar el precio con un valor por defecto en 0 
+(cero). 
+6.3. En la tabla de álbumes el nombre del título no puede ser nulo. */
 
